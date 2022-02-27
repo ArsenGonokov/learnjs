a83989c3f785682525eaffc9e8bc31954adec6a5
ArsenGonokov <arsen.gonokov@gmail.com>
2022-02-27 19:13:14 +0300

    #42.1

--- script.js
+++ script.js
@@ -20,7 +20,35 @@
 // }
 
 
-function calc (math, a, b) {
-    
-}
+function calc (math, a, b){
+    if (math == 'sum') {
+        console.log (a + b);
+    }
+    if (math == 'sub') {
+        console.log (a-b);
+    }
+    if (math == 'multi') {
+        console.log (a*b);
+    }
+    if (math == 'div') {
+        if (b == 0){
+            console.log ('На ноль делить нельзя сука!')
+        } else {
+            console.log (a/b);
+        }  
+    }
+    if (math == null) {
+        console.log ("Введите корректное значение!")
+    }
+    if (math == undefined) {
+        console.log ("Что черт возьми ты такое делаешь?")
+    } 
 
+}
+calc('sum', 3, 3);
+calc('sub', 3, 2);
+calc('div', 6, 2);
+calc('multi', 2, 2);
+calc('div', 2, 0);
+calc(null || null || null);
+calc(undefined || undefined || undefined)
