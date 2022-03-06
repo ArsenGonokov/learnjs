263fc7e3b2d67da2695ede1bcf5b9239c6940bfe
ArsenGonokov <arsen.gonokov@gmail.com>
2022-03-06 23:27:51 +0300

    #44.1

--- script.js
+++ script.js
@@ -20,35 +20,52 @@
 // }
 
 
-function calc (math, a, b){
-    if (typeof math !== "string" || typeof math === "" || typeof !a === "number" || typeof !b === "number") {
-        console.log("Error");
-    }
-    if (math == 'sum') {
-        console.log (a + b);
-    }
-    if (math == 'sub') {
-        console.log (a-b);
-    }
-    if (math == 'multi') {
-        console.log (a*b);
-    }
-    if (math == 'div') {
-        if (b == 0){
-            console.log ('На ноль делить нельзя сука!')
-        } else {
-            console.log (a/b);
-        }  
-    }
-    if (math === null) {
-        console.log ("Введите корректное значение!")
-    }
+// function calc (math, a, b){
+//     if (typeof math !== "string" || typeof math === "" || typeof !a === "number" || typeof !b === "number") {
+//         console.log("Error");
+//     }
+//     if (math == 'sum') {
+//         console.log (a + b);
+//     }
+//     if (math == 'sub') {
+//         console.log (a-b);
+//     }
+//     if (math == 'multi') {
+//         console.log (a*b);
+//     }
+//     if (math == 'div') {
+//         if (b == 0){
+//             console.log ('На ноль делить нельзя сука!')
+//         } else {
+//             console.log (a/b);
+//         }  
+//     }
+//     if (math === null) {
+//         console.log ("Введите корректное значение!")
+//     }
 
+// }
+// calc('sum', 3, 3);
+// calc('sub', 3, 2);
+// calc('div', 6, 2);
+// calc('multi', 2, 2);
+// calc('div', 2, 0);
+// calc(null || null || null);
+// calc();`
+
+
+
+    let math= "sum";
+    let a=1;
+    let b=3;
+    switch (math) {
+        case "sum":
+            console.log(a+b);
+            break;
+        case "sub":
+            console.log(a-b);
+            break;
+            default:
+                console.log("ХЗ что за хуйню ты хочешь!");
 }
-calc('sum', 3, 3);
-calc('sub', 3, 2);
-calc('div', 6, 2);
-calc('multi', 2, 2);
-calc('div', 2, 0);
-calc(null || null || null);
-calc();
+
