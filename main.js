21189cdf77d9a0f3ef7e43f65be9c708c0ae2b4e
Arsen Gonokov <arsen.gonokov@gmail.com>
2022-03-20 18:31:28 +0300

    #49.1

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-let rocket;
-rocket = "buran";
-console.log (rocket);
+'use strict';
+// let rocket;
+// rocket = "buran";
+// console.log (rocket);
+
+// console.log("вася"[0].toUpperCase());
+
+// let usFirst = str[0].toUpperCase() + str.slice(1);
+
+function usFirst(str){
+    if (!str) return str;
+    return str[0].toUpperCase() + str.slice(1);
+}
+
+console.log(usFirst("вася"));
